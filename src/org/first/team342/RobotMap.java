de6770e0d8ada22fc063df50ac875fd1469f8b8b
Team 342 <Team 342@team342-laptop>
2013-01-31 22:11:15 -0500

    just the basic additions

--- src/org/first/team342/RobotMap.java
+++ src/org/first/team342/RobotMap.java
@@ -8,8 +8,13 @@ package org.first.team342;
  */
 public class RobotMap {
 
+    //drive motor addresses
     public static final int CAN_DEVICE_RIGHT_FRONT_DRIVE_MOTOR = 1;
     public static final int CAN_DEVICE_LEFT_FRONT_DRIVE_MOTOR = 2;
     public static final int CAN_DEVICE_LEFT_REAR_DRIVE_MOTOR = 3;
     public static final int CAN_DEVICE_RIGHT_REAR_DRIVE_MOTOR = 4;
+    //controller location
+    public static final int JOYSTICK_DRIVE_CONTROL = 1;
+    //Default position in the CRIO for the side car
+    public static final int DEFAULT_ANNALOG_SLOT = 1;
 }
