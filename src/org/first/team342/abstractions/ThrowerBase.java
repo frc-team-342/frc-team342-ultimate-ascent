a572cdf362aee73ff5d43836059dc519df07a60d
ryan-becwar <rbecwar1@gmail.com>
2013-02-17 19:41:17 -0500

    update to thrower base, move to angle command

--- src/org/first/team342/abstractions/ThrowerBase.java
+++ src/org/first/team342/abstractions/ThrowerBase.java
@@ -21,7 +21,6 @@ package org.first.team342.abstractions;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.first.team342.RobotMap;
 
 /**
@@ -29,7 +28,7 @@ import org.first.team342.RobotMap;
  * @author Team 342
  */
 public abstract class ThrowerBase extends Subsystem implements Thrower {
-
+    
     private double targetAngle;
     private SpeedController aim;
 //    private Gyro gyro;
@@ -39,7 +38,8 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
     protected AnalogChannel potentiometer;
     protected DigitalInput pushLimitSwitch;
     protected Timer timer;
-
+    protected DriverStation driver;
+    
     protected ThrowerBase() {
         super();
         this.targetAngle = 0.0;
@@ -50,6 +50,7 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
         this.pushMotor = new Victor(RobotMap.PWM_CHANNEL_FIRE);
         this.pushLimitSwitch = new DigitalInput(RobotMap.DIO_CHANNEL_FIRE);
         this.potentiometer = new AnalogChannel(RobotMap.ANALOG_CHANNEL_POTENTIOMETER);
+        this.driver = DriverStation.getInstance();
     }
 
 //    public void moveToAngle(double angle) {
@@ -104,20 +105,25 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
             this.aim.set(speed);
         }
     }
-
+    
     public void moveToAngle(double angle) {
-        double uncertainty = 1.0;
-        if (!this.top.get()) {
-            this.aim.set(0.0);
-        } else if (!this.bottom.get()) {
-            this.aim.set(0.0);
-        } else if (this.convertAngle(this.potentiometer.getVoltage()) + uncertainty < angle) {
-            this.aim.set(.5);
-        } else if (this.convertAngle(this.potentiometer.getVoltage()) - uncertainty > angle) {
-            this.aim.set(-.5);
-        } else {
-            this.aim.set(0.0);
+        /*
+         * double uncertainty = 1.0; if (!this.top.get()) { this.aim.set(0.0); }
+         * else if (!this.bottom.get()) { this.aim.set(0.0); } else if
+         * (this.convertAngle(this.potentiometer.getVoltage()) + uncertainty <
+         * angle) { this.aim.set(.5); } else if
+         * (this.convertAngle(this.potentiometer.getVoltage()) - uncertainty >
+         * angle) { this.aim.set(-.5); } else { this.aim.set(0.0); }
+         */
+        double aimOut = (this.driver.getAnalogIn(3) / 10) * (angle - this.convertAngle());
+        if (aimOut > 1){
+            aimOut = 1;
         }
+       if (aimOut < .3) {
+            aimOut = .3;
+        }
+       this.aim.set(-aimOut);
+       System.out.println("Aim Out :" + aimOut);
     }
 
     //TODO: fix target angle settings
@@ -149,15 +155,15 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
         }
     }
     
-    public void potentiometerTest(){
+    public void potentiometerTest() {
         double rawValue = this.potentiometer.getVoltage();
         System.out.println("Raw Value: " + rawValue);
 //        SmartDashboard.putNumber("Raw Value: ", rawValue);
         double angleValue = this.convertAngle(rawValue);
 //        SmartDashboard.putNumber("Angle Value: ", angleValue);
         System.out.println("Angle Value: " + angleValue);
-}
-
+    }
+    
     public void simpleLower(double speed) {
         if (!this.bottom.get()) {
             this.aim.set(0.0);
@@ -166,16 +172,16 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
             this.aim.set(-speed);
         }
     }
-
+    
     public void aimMotorStop() {
         this.aim.set(0.0);
     }
-
+    
     public void pushMotorSet(double value) {
         pushMotor.set(value);
         System.out.println("vex motor running");
     }
-
+    
     public void push(double value) {
         if (pushLimitSwitch.get()) {
             pushMotor.set(value);
@@ -183,20 +189,19 @@ public abstract class ThrowerBase extends Subsystem implements Thrower {
             pushMotor.set(0.0);
         }
     }
-
+    
     public boolean getSwitch() {
         return pushLimitSwitch.get();
     }
-
+    
     public double convertAngle(double input) {
         double angleConstant = 60.0;
         double angle = input * angleConstant;
         return angle;
     }
-    
+
     //JH - Added method that gets pot volts on its own
-    
-    public double convertAngle(){
+    public double convertAngle() {
         double angleConstant = 60.;//JH - Move with the other constants
         double angle = angleConstant * this.potentiometer.getVoltage();
         return angle;
