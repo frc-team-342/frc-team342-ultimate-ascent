b2229be42d216a4ca9a2e2abddbf387c1c3b33e1
ryan-becwar <rbecwar1@gmail.com>
2013-02-17 19:40:12 -0500

    modifications

---
+++ src/org/first/team342/commands/thrower/PushStopCommand.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.commands.thrower;
+
+import org.first.team342.abstractions.ThrowerBase;
+import org.first.team342.commands.CommandBase;
+import org.first.team342.subsystems.ThrowerPWM;
+
+/**
+ *
+ * @author Charlie
+ */
+public class PushStopCommand extends CommandBase {
+    private ThrowerBase thrower = ThrowerPWM.getInstance();
+    
+    public PushStopCommand() {
+        requires(thrower);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        this.thrower.pushMotorSet(0.0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
